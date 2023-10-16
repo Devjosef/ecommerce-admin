712c1f9392a16240bf93ca458d6d3a4788ae744c
Devjosef <dev.josef1@gmail.com>
2023-10-16 08:57:18 +0200

    origin,update,schema update

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -26,6 +26,7 @@ import { Input } from "@/components/ui/input";
 import toast from "react-hot-toast";
 import { AlertModal } from "@/components/modals/alert-modal";
 import { ApiAlert } from "@/components/ui/api-alert";
+import { useOrigin } from "@/hooks/use-origin";
 
 
 interface SettingsFormProps {
@@ -43,6 +44,9 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 }) => {
     const params = useParams();
     const router = useRouter();
+    const origin = useOrigin();
+    
+    
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
     
@@ -127,7 +131,7 @@ const onDelete = async () => {
         <Separator />
         <ApiAlert 
         title="NEXT_PUBLIC_API_URL" 
-        description="test-desc" 
+        description={`${origin}/api/${params.storeId}`} 
         variant="public" 
         />
         </>
