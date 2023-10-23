ba4b1619e93090acae54147c4ec44510e71a79a7
Devjosef <dev.josef1@gmail.com>
2023-10-23 10:57:26 +0200

    Edited: added data table, and billboard.

--- app/(dashboard)/[storeId]/(routes)/billboards/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/client.tsx
@@ -6,17 +6,24 @@ import { useParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { BillboardColumn, columns } from "./[billboardId]/components/columns";
+import { DataTable } from "@/components/ui/data-table";
 
 
+interface BillboardClientProps {
+   data: BillboardColumn[]
+}
 
-export const BillboardClient = () => {
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+   data
+}) => {
     const router = useRouter();
     const params = useParams();
     return (
     <>
     <div className="flex items-center justify-between">
      <Heading
-     title="Billboards (0)"
+     title={`Billboards (${data.length})`}
      description="Manage billboards for your store" 
      />
      <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
@@ -25,6 +32,7 @@ export const BillboardClient = () => {
      </Button>
     </div>
     <Separator />
+    <DataTable columns={columns} data={data} />
     </>
  )
 }
