4adcc7d39abe3220ef50ce7bb771faa8c8ecb61e
Devjosef <dev.josef1@gmail.com>
2023-10-27 09:29:06 +0200

    Edit: first part colors. api routes next.

---
+++ app/(dashboard)/[storeId]/(routes)/colors/page.tsx
@@ -0,0 +1,38 @@
+import { format } from "date-fns";
+
+import prismadb from "@/lib/prismadb";
+import { ColorsClient } from "./client";
+import { ColorColumn} from "./[colorId]/components/columns";
+
+const ColorsPage = async ({
+    params
+}: {
+    params: { storeId: string }
+}) => {
+    const colors = await prismadb.size.findMany({
+        where: {
+            storeId: params.storeId
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
+
+    const formattedColors: ColorColumn[] = colors.map((item) => ({
+        id: item.id,
+        name: item.name,
+        value: item.value, 
+        createdAt: format(item.createdAt, "MMMM do, yyyy")
+    }))
+    
+    return (
+        <div className="flex-col">
+        <div className="flex-1 space-y-4 p-8 pt-6">
+            <ColorsClient data={formattedColors} />
+        
+        </div>
+        </div>
+    );
+}
+
+export default ColorsPage;
