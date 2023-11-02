358e44d3c4823e7ff67c6f4cfce31a08b13efb4e
Devjosef <dev.josef1@gmail.com>
2023-11-02 08:30:19 +0100

    Edit: continuation p-form.tsx

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -3,14 +3,14 @@
 import * as z from "zod";
 import axios from "axios";
 import { useState } from "react";
-import { Image, Product } from "@prisma/client";
+import { Category, Color, Image, Product, Size } from "@prisma/client";
 import { Trash } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useParams, useRouter } from "next/navigation";
 
 
-
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Heading } from "@/components/ui/heading";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -26,12 +26,16 @@ import { Input } from "@/components/ui/input";
 import toast from "react-hot-toast";
 import { AlertModal } from "@/components/modals/alert-modal";
 import ImageUpload from "@/components/ui/image-upload";
+import { url } from "inspector";
 
 
 interface ProductFormProps {
     initialData: Product  & {
         images: Image[]
     }| null;
+    categories: Category[];
+    colors: Color[];
+    sizes: Size[];
 }
 
 const formSchema = z.object ({
@@ -48,7 +52,10 @@ const formSchema = z.object ({
 type ProductFormValues = z.infer<typeof formSchema>;
 
 export const ProductForm: React.FC<ProductFormProps> = ({
-    initialData
+    initialData,
+    categories,
+    colors,
+    sizes,
 }) => {
     const params = useParams();
     const router = useRouter();
@@ -142,13 +149,131 @@ const onDelete = async () => {
         <form  onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
         <FormField 
         control={form.control}
-        name="label"
+        name="name"
         render={({ field }) => (
             <FormItem>
-                <FormLabel>Label</FormLabel>
+                <FormLabel>Name</FormLabel>
                 <FormControl>
-                    <Input disabled={loading} placeholder="Billboard label" {...field} />
+                    <Input disabled={loading} placeholder="Product name" {...field} />
                 </FormControl>
+                <FormMessage />
+            </FormItem>
+        )}
+        />
+           <FormField 
+        control={form.control}
+        name="price"
+        render={({ field }) => (
+            <FormItem>
+                <FormLabel>Price</FormLabel>
+                <FormControl>
+                    <Input type="number" disabled={loading} placeholder="12.99" {...field} />
+                </FormControl>
+                <FormMessage />
+            </FormItem>
+        )}
+        />
+         <FormField 
+        control={form.control}
+        name="categoryId"
+        render={({ field }) => (
+            <FormItem>
+                <FormLabel>Category</FormLabel>
+                <Select 
+                disabled={loading} 
+                onValueChange={field.onChange} 
+                value={field.value} 
+                defaultValue={field.value}
+                >
+                <FormControl>
+                    <SelectTrigger>
+                    <SelectValue 
+                    defaultValue={field.value}
+                    placeholder="Select a Category"
+                    />
+                    </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                    {categories.map((category) => (
+                        <SelectItem
+                        key={category.id}
+                        value={category.id}
+                        >
+                         {category.name}
+                        </SelectItem>
+                    ))}
+                </SelectContent>
+                </Select>
+                <FormMessage />
+            </FormItem>
+        )}
+        />
+         <FormField 
+        control={form.control}
+        name="sizeId"
+        render={({ field }) => (
+            <FormItem>
+                <FormLabel>Size</FormLabel>
+                <Select 
+                disabled={loading} 
+                onValueChange={field.onChange} 
+                value={field.value} 
+                defaultValue={field.value}
+                >
+                <FormControl>
+                    <SelectTrigger>
+                    <SelectValue 
+                    defaultValue={field.value}
+                    placeholder="Select a Size"
+                    />
+                    </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                    {sizes.map((size) => (
+                        <SelectItem
+                        key={size.id}
+                        value={size.id}
+                        >
+                         {size.name}
+                        </SelectItem>
+                    ))}
+                </SelectContent>
+                </Select>
+                <FormMessage />
+            </FormItem>
+        )}
+        />
+         <FormField 
+        control={form.control}
+        name="colorId"
+        render={({ field }) => (
+            <FormItem>
+                <FormLabel>Color</FormLabel>
+                <Select 
+                disabled={loading} 
+                onValueChange={field.onChange} 
+                value={field.value} 
+                defaultValue={field.value}
+                >
+                <FormControl>
+                    <SelectTrigger>
+                    <SelectValue 
+                    defaultValue={field.value}
+                    placeholder="Select a Color"
+                    />
+                    </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                    {colors.map((color) => (
+                        <SelectItem
+                        key={color.id}
+                        value={color.id}
+                        >
+                         {color.name}
+                        </SelectItem>
+                    ))}
+                </SelectContent>
+                </Select>
                 <FormMessage />
             </FormItem>
         )}
@@ -156,22 +281,23 @@ const onDelete = async () => {
         <div className="grid grid-cols-3 gap-8">
         <FormField 
         control={form.control}
-        name="imageUrl"
+        name="images"
         render={({ field }) => (
             <FormItem>
-                <FormLabel>Background image</FormLabel>
+                <FormLabel>Images</FormLabel>
                 <FormControl>
                     <ImageUpload
-                    value={field.value ? [field.value] : []}
+                    value={field.value.map((image) => image.url )}
                     disabled={loading}
-                    onChange={(url) => field.onChange(url)}
-                    onRemove={() => field.onChange("")}
+                    onChange={(url) => field.onChange([...field.value, { url }])}
+                    onRemove={(url) => field.onChange([...field.value.filter((current) => current.url !== url)])}
                     />
                 </FormControl>
                 <FormMessage />
             </FormItem>
         )}
         />
+        
         </div>
        <Button disabled={loading} className="ml-auto" type="submit">
         {action}
