bc71b2c3647abffe6c43318b74a96395efbb1ad2
Devjosef <dev.josef1@gmail.com>
2023-11-07 09:11:27 +0100

    Edit: Admin complete, opening actual store.

--- app/(dashboard)/[storeId]/(routes)/orders/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/client.tsx
@@ -1,42 +1,26 @@
 "use client";
 
-import { Plus } from "lucide-react";
-import { useParams, useRouter } from "next/navigation";
-
-import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { BillboardColumn, columns } from "./[billboardId]/components/columns";
 import { DataTable } from "@/components/ui/data-table";
-import { ApiList } from "@/components/ui/api-list";
 
+import { OrderColumn, columns } from "./components/columns"
 
-interface BillboardClientProps {
-   data: BillboardColumn[]
+interface OrderClientProps {
+   data: OrderColumn[]
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({
+export const OrderClient: React.FC<OrderClientProps> = ({
    data
 }) => {
-    const router = useRouter();
-    const params = useParams();
     return (
     <>
-    <div className="flex items-center justify-between">
      <Heading
-     title={`Billboards (${data.length})`}
-     description="Manage billboards for your store" 
+     title={`Orders (${data.length})`}
+     description="Manage orders for your store" 
      />
-     <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
-        <Plus className="mr-2 h-4 w-4" />
-        Add New
-     </Button>
-    </div>
     <Separator />
-    <DataTable searchKey="label" columns={columns} data={data} />
-    <Heading title="API" description="API calls for Billboards" />
-    <Separator />
-    <ApiList entityName="billboards" entityIdName="billboardId" />
+    <DataTable searchKey="products" columns={columns} data={data} />
     </>
  )
 }
--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -1,8 +1,8 @@
 import { format } from "date-fns";
 
 import prismadb from "@/lib/prismadb";
-import { BillboardClient } from "./client";
-import { BillboardColumn } from "./[billboardId]/components/columns";
+import { OrderClient } from "./client";
+import { OrderColumn } from "./components/columns";
 import { formatter } from "@/lib/utils";
 
 const OrdersPage = async ({
@@ -10,11 +10,11 @@ const OrdersPage = async ({
 }: {
     params: { storeId: string }
 }) => {
-    const orders = await prismadb..findMany({
+    const orders = await prismadb.order.findMany({
         where: {
             storeId: params.storeId
         },
-        inlude: {
+        include: {
             orderItems: {
                 include: {
                     product: true
@@ -26,25 +26,26 @@ const OrdersPage = async ({
         }
     });
 
-    const formattedOrders: BillboardColumn[] = orders.map((item) => ({
+    const formattedOrders: OrderColumn[] = orders.map((item) => ({
         id: item.id,
         phone: item.phone,
-        address: item.adress,
+        address: item.address,
         products: item.orderItems.map((orderItem) => orderItem.product.name).join(', '),
         totalPrice: formatter.format(item.orderItems.reduce((total, item) => {
             return total + Number(item.product.price)
         }, 0)),
+        isPaid: item.isPaid,
         createdAt: format(item.createdAt, "MMMM do, yyyy")
     }))
     
     return (
         <div className="flex-col">
         <div className="flex-1 space-y-4 p-8 pt-6">
-            <BillboardClient data={formattedBillboards} />
+            <OrderClient data={formattedOrders} />
         
         </div>
         </div>
     );
 }
 
-export default BillboardsPage;
+export default OrdersPage;
