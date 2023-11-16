9e3a6e9236da9442803a01012f87eaaa30b87ea3
Devjosef <dev.josef1@gmail.com>
2023-11-16 07:26:04 +0100

    Edit: Moved colors to storeId

--- app/api/[storeId]/colors/route.ts
+++ app/api/[storeId]/colors/route.ts
@@ -5,7 +5,7 @@ import prismadb from "@/lib/prismadb";
 
 export async function POST(
     req: Request,
-    { params }: {params: {storeId: string } }
+    { params }: { params: { storeId: string } }
 ) {
     try {
        const { userId } = auth();
@@ -14,19 +14,19 @@ export async function POST(
        const { name, value } = body;
         
        if (!userId) {
-            return new NextResponse("Unauthenticated", { status: 401});
+            return new NextResponse("Unauthenticated", { status: 401 });
         }
     
         if (!name) {
-            return new NextResponse("Name is required", { status: 400});
+            return new NextResponse("Name is required", { status: 400 });
         }
 
         if (!value) {
-            return new NextResponse("Value is required", { status: 400});
+            return new NextResponse("Value is required", { status: 400 });
         }
 
         if (!params.storeId) {
-            return new NextResponse("Store id is required", { status: 400});
+            return new NextResponse("Store id is required", { status: 400 });
         }
 
         const storeByUserId = await prismadb.store.findFirst({
@@ -49,12 +49,12 @@ export async function POST(
         });
 
         return NextResponse.json(color);
-    } catch (error) {
+    }   catch (error) {
         console.log('[COLORS_POST]', error);
-        return new NextResponse("Internal error", { status: 500});
+        return new NextResponse("Internal error", { status: 500 });
     }
 
-    };
+};
 
     export async function GET(
         req: Request,
@@ -62,7 +62,7 @@ export async function POST(
     ) {
         try {
           if (!params.storeId) {
-                return new NextResponse("Store id is required", { status: 400});
+                return new NextResponse("Store id is required", { status: 400 });
             }
 
             const colors = await prismadb.color.findMany({
@@ -74,7 +74,6 @@ export async function POST(
             return NextResponse.json(colors);
         } catch (error) {
             console.log('[COLORS_GET]', error);
-            return new NextResponse("Internal error", { status: 500});
+            return new NextResponse("Internal error", { status: 500 });
         }
-    
-        };
+     };
